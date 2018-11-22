614cfbae7a67fa9d23673441139d7a6fbb3b639b
VictorCL123 <cuevas-17@live.com.mx>
2018-11-22 03:23:03 -0600

    Se agrega interfaz de recuperar contraseña, se agrega la opcion de cancela

--- RecuperarContrasena.php
+++ RecuperarContrasena.php
@@ -1,6 +1,7 @@
 <!doctype html>
 
 <?php
+header("Content-Type: text/html;charset=utf-8");
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
@@ -38,12 +39,13 @@ if($_SERVER['REQUEST_METHOD'] == 'POST')
                 $mail->addAddress($email);
                 //Content
                 $mail->isHTML(true);
-                $mail->Subject = 'Recuperaración de contraseña';
+                $mail->Subject = 'Recuperaraci&oacute;n de contraseña';
                 $mail->Body    = 'Su contraseña es: <b>' . $password . '</b>';
                 $mail->AltBody = 'Su contraseña es: ' . $password;
 
                 $mail->send();
                 echo 'Message has been sent';
+                $x = 1;
             }
             catch (Exception $e)
             {
@@ -53,23 +55,57 @@ if($_SERVER['REQUEST_METHOD'] == 'POST')
     }
     // Se muestra el mensaje de que el correo ha sido enviado sea o no email
     // Esto se hace para proteger la BD
+    // Esto no lo puedes hacer por que tienes un try-cath, ademas de que muchas paginas ayudan al cliente avisandole si el correo fue encontrado
 }
 ?>
-
 <html>
 <head>
     <title>Contrase&ntilde;a - CECyTEM Tequixquiac</title>
+    <link rel="stylesheet" type="text/css" href="CSSEstilos/Index.css"/>
+    <link rel="stylesheet" type="text/css" href="CSSEstilos/RecuperarContraeña.css"/>
 </head>
 
 <body>
-    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-        <input name="email" type="email" maxlength="40" placeholder="Correo electr&oacute;nico">
-        <br>
-        <br>
-        <input type="submit" value="Recuperar Contrase&ntilde;a">
-    </form>
-    <?php echo '<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>'; ?>
+  <div class="header">
+      <img src="Imagenes/GobEdoMex.png" style="height:100px;width:300px;float:left;">
+      <img src="Imagenes/CECyTEM.png" style="height:100px;width:100px;float:right;">
+  </div>
+    <div class="aplicacion-main" style="height:400px;padding-top:1px;padding-bottom:1px;">
+        <div class="contenedor" align="center" style="margin-top:5%">
+            <div class="formato" style="max-width:300px;">
+                <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+                    <dl class="grupoconformato">
+                        <dt class="imput-label">
+                            <label class="formato-label1">
+                                <font style="vertical-aling:inherit;">
+                                Email
+                                </font>
+                            </label>
+                        </dt>
+                        <dd style="margin:0;width:90%;">
+                            <input name="email" type="email" maxlength="40" placeholder="Correo electr&oacute;nico" class="formato-imput">
+                        </dd>
+                    </dl>
+                    <dl class="grupoconformato">
+                        <dd style="margin:0;width:90%;">
+                            <input type="submit" value="Recuperar Contrase&ntilde;a" class="block">
 
-    <!-- Regresar a index.php -->
+                        </dd>
+                        <dd style="margin:0;width:90%;">
+                            <!--boton para reresar a la ventana de inicio-->
+                            <input type="button" value="Cancelar" onclick="funcion1()" class="block" style="margin-top:5px;">
+                        </dd>
+                    </dl>
+                </form>
+            <?php echo '<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>'; ?>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
+
+<script>
+function funcion1() {
+    location.href="PHPFunciones/CerrarSesion.php";
+}
+</script>
