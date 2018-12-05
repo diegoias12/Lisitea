7d8a27f6c487cd8999b4f5354e515847f5b19f11
cuevas-17@live.com.mx <cuevas-17@live.com.mx>
2018-12-05 12:01:13 -0600

    Menu de inicio filtrado

--- Inicio.php
+++ Inicio.php
@@ -11,94 +11,18 @@
     <script type="text/javascript" src="JavaScriptFunciones/Menu.js"></script>
     <script type="text/javascript" src="JavaScriptFunciones/Planeacion.js"></script>
     <script type="text/javascript" src="JavaScriptFunciones/SqlSelectElement.js"></script>
+    <script type="text/javascript" src="jQueryAjax/ObtenerValor.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 </head>
 
 <body>
     <div id="Menu">
         <?php require 'PHPInclude/Menu.php'; ?>
     </div>
+    <script>FiltrarMenu('tbl_usuario','FK_id_tipo_usuario')</script>
     <div id="Contenido">
 
     </div>
-    <script>
-        /*
-        function Funcion1(){
-          document.getElementById('PagRevision').style.display = "block";
-          document.getElementById('PagRevision2').style.display = "block";
-          document.getElementById('PagInicioPE').style.display = "none";
-          document.getElementById('PagInicioPE2').style.display = "none";
-          document.getElementById('PagAdministracion').style.display = "none";
-          document.getElementById('PagAdministracion2').style.display = "none";
-          document.getElementById('PagUsuarios').style.display = "none";
-          document.getElementById('PagUsuarios').style.display = "none";
-        }
-        function Funcion2(){
-          document.getElementById('PagRevision').style.display = "none";
-          document.getElementById('PagRevision2').style.display = "none";
-          document.getElementById('PagInicioPE').style.display = "none";
-          document.getElementById('PagInicioPE2').style.display = "none";
-          document.getElementById('PagAdministracion').style.display = "block";
-          document.getElementById('PagAdministracion2').style.display = "block";
-          document.getElementById('PagUsuarios').style.display = "block";
-          document.getElementById('PagUsuarios').style.display = "block";
-        }
-        function Funcion3(){
-          document.getElementById('PagRevision').style.display = "none";
-          document.getElementById('PagRevision2').style.display = "none";
-          document.getElementById('PagInicioPE').style.display = "block";
-          document.getElementById('PagInicioPE2').style.display = "block";
-          document.getElementById('PagAdministracion').style.display = "none";
-          document.getElementById('PagAdministracion2').style.display = "none";
-          document.getElementById('PagUsuarios').style.display = "none";
-          document.getElementById('PagUsuarios').style.display = "none";
-        }
-        function Funcion4(){
-          document.getElementById('PagRevision').style.display = "none";
-          document.getElementById('PagRevision2').style.display = "none";
-          document.getElementById('PagInicioPE').style.display = "none";
-          document.getElementById('PagInicioPE2').style.display = "none";
-          document.getElementById('PagAdministracion').style.display = "block";
-          document.getElementById('PagAdministracion2').style.display = "block";
-          document.getElementById('PagUsuarios').style.display = "none";
-          document.getElementById('PagUsuarios').style.display = "none";
-        }
-        function Funcion5(){
-          document.getElementById('PagRevision').style.display = "none";
-          document.getElementById('PagRevision2').style.display = "none";
-          document.getElementById('PagInicioPE').style.display = "none";
-          document.getElementById('PagInicioPE2').style.display = "none";
-          document.getElementById('PagAdministracion').style.display = "none";
-          document.getElementById('PagAdministracion2').style.display = "none";
-          document.getElementById('PagUsuarios').style.display = "none";
-          document.getElementById('PagUsuarios').style.display = "none";
-        }*/
-        var x = 1
-        if(x==1)
-        {
-            const list = document.getElementsByClassName("PagRevision");
-            list[0].style.display = "block";
-        }
-        //var x = 2;
-        if(x==2)
-        {
-            document.getElementById('PagAdministracion').style.display = "block";
-            document.getElementById('PagAdministracion2').style.display = "block";
-            document.getElementById('PagUsuarios').style.display = "block";
-            document.getElementById('PagUsuarios2').style.display = "block";
-        }
-        var x = 3;
-        if(x==3)
-        {
-            document.getElementById('PagInicioPE').style.display = "block";
-            document.getElementById('PagInicioPE2').style.display = "block";
-        }
-        //var x = 4;
-        if(x==4)
-        {
-            document.getElementById('PagAdministracion').style.display = "block";
-            document.getElementById('PagAdministracion2').style.display = "block";
-        }
-    </script>
     <!-- En cso de requerir un cambio de contraseña
     <div id="Menu2">
         <?php //require 'PHPInclude/SolicitarCambioContraseña.php'; ?>
